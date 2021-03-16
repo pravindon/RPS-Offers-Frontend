57afabf677ba6d82dc92f7d88edd3804686633ec
unknown <pdpravin47@gmail.com>
2021-03-16 08:31:20 +0530

    pack file up

--- server.js
+++ server.js
@@ -4,13 +4,25 @@ const path = require('path');
 
 const app = express();
 
+const forceSSL = function() {
+  return function (req, res, next) {
+    if (req.headers['x-forwarded-proto'] !== 'https') {
+      return res.redirect(
+        ['https://', req.get('Host'), req.url].join('')
+      );
+    }
+    next();
+  }
+}
+
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/RPS-FRONTEND'));
+app.use(express.static(__dirname + '/dist/'));
 
 app.get('/*', function(req,res) {
-
-res.sendFile(path.join(__dirname+'/dist/RPS-FRONTEND/index.html'));
+res.sendFile(path.join(__dirname+'/dist/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080);
+app.listen(process.env.PORT || 8080, () => {
+  console.log('application started at port',server.address().port);
+});
